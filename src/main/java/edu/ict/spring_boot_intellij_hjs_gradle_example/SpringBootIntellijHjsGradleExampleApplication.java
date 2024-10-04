43e3cc2aeae833d2398a91bcbc311fabb5cbf0bd
TEACHER\ICT02_22 <admin@admin.com>
2024-10-04 18:18:36 +0900

    Initial commit

---
+++ src/main/java/edu/ict/spring_boot_intellij_hjs_gradle_example/SpringBootIntellijHjsGradleExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.spring_boot_intellij_hjs_gradle_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntellijHjsGradleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntellijHjsGradleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ict/spring_boot_intellij_hjs_gradle_example/SpringBootIntellijHjsGradleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ict.spring_boot_intellij_hjs_gradle_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIntellijHjsGradleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
